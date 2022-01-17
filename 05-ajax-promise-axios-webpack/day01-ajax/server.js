173b20bb8f2b67cfc8edb058b9d9047c1b3d78d4
junjieweb <297901657@qq.com>
2022-01-17 15:00:55 +0800

    提交已有代码

--- 05-ajax-promise-axios-webpack/day01-ajax/server.js
+++ 05-ajax-promise-axios-webpack/day01-ajax/server.js
@@ -25,7 +25,14 @@ app.get('/test_get2/:name/:age', (request, response) => {
     response.send('hello_test_get2');
 });
 
-//响应POST请求2--可以接收请求体参数
+//响应GET请求3
+app.get('/get_person', (request, response) => {
+    console.log('有人请求get_person了');
+    const person = {name: 'tom', age: 19, sex: '男'};
+    response.send(JSON.stringify(person));
+});
+
+//响应POST请求--可以接收请求体参数
 app.post('/test_post', (request, response) => {
     console.log('有人请求test_post了--携带的请求体参数是：', request.body);
     response.send('hello_test_post');
@@ -38,5 +45,6 @@ app.listen(8080, (err) => {
         console.log('http://127.0.0.1:8080/2-xhr的5种状态.html');
         console.log('http://127.0.0.1:8080/3-ajax-get请求.html');
         console.log('http://127.0.0.1:8080/4-ajax-post请求.html');
+        console.log('http://127.0.0.1:8080/5_ajax解析json数据.html');
     }
 });
