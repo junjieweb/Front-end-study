b9e2498c422c479e2805e206a20257513faa3dbe
junjieweb <297901657@qq.com>
2022-01-17 16:36:28 +0800

    更新代码

--- 05-ajax-promise-axios-webpack/day01-ajax/server.js
+++ 05-ajax-promise-axios-webpack/day01-ajax/server.js
@@ -57,5 +57,6 @@ app.listen(8080, (err) => {
         console.log('http://127.0.0.1:8080/5_ajax解析json数据.html');
         console.log('http://127.0.0.1:8080/6-ajax处理ie-get请求缓存问题.html');
         console.log('http://127.0.0.1:8080/7-ajax请求异常与超时处理.html');
+        console.log('http://127.0.0.1:8080/8-ajax取消请求.html');
     }
 });
