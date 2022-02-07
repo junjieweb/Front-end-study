07801def5d1b2eea4fbe57e9d3c8ee4905f28edf
junjieweb <297901657@qq.com>
2022-02-07 16:40:11 +0800

    add code

--- 06-react/2-react-stagging/src/app.jsx
+++ 06-react/2-react-stagging/src/app.jsx
@@ -1,10 +1,35 @@
 import React, {Component} from 'react';
+import axios from "axios";
 
 class App extends Component {
+
+    getStudentList = () => {
+        axios.get('http://localhost:3000/students').then(
+            response => {
+                console.log('成功了', response)
+            },
+            error => {
+                console.log('失败了', error)
+            }
+        )
+    }
+
+    getCarList = () => {
+        axios.get('http://localhost:3000/cars').then(
+            response => {
+                console.log('成功了', response)
+            },
+            error => {
+                console.log('失败了', error)
+            }
+        )
+    }
+
     render() {
         return (
             <div>
-                App
+                <button onClick={this.getStudentList}>点我获取一些学生的数据</button>
+                <button onClick={this.getCarList}>点我获取一些汽车的数据</button>
             </div>
         );
     }
