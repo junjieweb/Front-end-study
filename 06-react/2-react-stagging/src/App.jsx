d454a5af823507f3985ca64f5256960095a41f72
junjieweb <297901657@qq.com>
2022-02-08 18:12:12 +0800

    add code

--- 06-react/2-react-stagging/src/App.jsx
+++ 06-react/2-react-stagging/src/App.jsx
@@ -1,13 +1,39 @@
 import React, {Component} from 'react';
-import Search from "./components/Seatch";
-import List from "./components/List";
+import Home from "./pages/Hmoe";
+import About from "./pages/About";
+import Header from "./components/Header";
+import {NavLink, Routes, Route} from 'react-router-dom'
 
 class App extends Component {
     render() {
         return (
-            <div className="container">
-                <Search/>
-                <List/>
+            <div>
+                <Header/>
+                <div className="row">
+                    <div className="col-xs-2 col-xs-offset-2">
+                        <div className="list-group">
+
+                            {/*原生html中，我们使用a标签进行页面的跳转*/}
+                            {/*<a className="list-group-item" href="./about.html">About</a>
+                            <a className="list-group-item" href="./home.html">Home</a>*/}
+
+                            {/*React中，使用Link进行路径的切换*/}
+                            <NavLink className="list-group-item" to="/about">About</NavLink>
+                            <NavLink className="list-group-item" to="/home">Home</NavLink>
+                        </div>
+                    </div>
+                    <div className="col-xs-6">
+                        <div className="panel">
+                            <div className="panel-body">
+                                {/*注册路由*/}
+                                <Routes>
+                                    <Route path="/about" element={<About/>}/>
+                                    <Route path="/home" element={<Home/>}/>
+                                </Routes>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
