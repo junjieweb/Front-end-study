3765e52014f4fd876c476ba26b00f7f9065d0276
junjieweb <297901657@qq.com>
2022-02-21 02:28:11 +0800

    add code

--- 8-vue/2_vue_components/src/App.vue
+++ 8-vue/2_vue_components/src/App.vue
@@ -1,12 +1,45 @@
 <template>
   <div>
-
+    <button @click="increment">点击加1</button>
+    <button @click="decrement">点击减1</button>
+    <button @click="incrementIfOdd">点击奇数加1</button>
+    <button @click="incrementAsync">点击异步+1</button>
+    <!--<p>{{ count }}</p>-->
+    <p>{{ $store.state.count }}</p>
   </div>
 </template>
 
 <script>
 export default {
-  name: "App"
+  name: "App",
+  /*data() {
+    return {
+      count: 0
+    }
+  },*/
+  methods: {
+    increment() {
+      // this.count++
+      //dispatch 和 emit 都是触发分发的意思，分发触发actions中对于的方法
+      this.$store.dispatch('increment')
+    },
+    decrement() {
+      // this.count--
+      this.$store.dispatch('decrement')
+    },
+    incrementIfOdd() {
+      /*if (this.count % 2 !== 0) {
+        this.count++
+      }*/
+      this.$store.dispatch('incrementIfOdd')
+    },
+    incrementAsync() {
+      /*setTimeout(() => {
+        this.count++
+      }, 1000)*/
+      this.$store.dispatch('incrementAsync')
+    }
+  }
 }
 </script>
 
--- 8-vue/2_vue_components/src/main.js
+++ 8-vue/2_vue_components/src/main.js
@@ -1,8 +1,12 @@
 import Vue from "vue";
 import App from "./App";
+import store from "./store";
 
 Vue.config.productionTip = false
 
 new Vue({
-    render: h => h(App)
-}).$mount('#app')
+    el: '#app',
+    render: h => h(App),
+    //store对象在配置对象中配置使用，每个组件对象中都可以通过this.$store获取到我们的这个对象
+    store
+})
