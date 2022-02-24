b36591a76be433f97d38f63d8d4c65f95ee814fc
junjieweb <297901657@qq.com>
2022-02-24 17:53:48 +0800

    add code

---
+++ 8-vue/shop-client/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    App组件
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
