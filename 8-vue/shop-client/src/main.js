2f0532bd1c81ef72736262d30e28093e93ac0cc6
junjieweb <297901657@qq.com>
2022-02-25 01:40:00 +0800

    add code

--- 8-vue/shop-client/src/main.js
+++ 8-vue/shop-client/src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from "@/router";
+import TypeNav from "@/components/TypeNav";
 
 Vue.config.productionTip = false
+
+//注册全局组件
+Vue.component(TypeNav.name, TypeNav)
 
 new Vue({
     render: h => h(App),
--- 8-vue/shop-client/src/pages/Home/index.vue
+++ 8-vue/shop-client/src/pages/Home/index.vue
@@ -1,15 +1,37 @@
 <template>
-  <div class="home">Home</div>
+  <div>
+    <TypeNav></TypeNav>
+    <ListContainer></ListContainer>
+    <TodayRecommend></TodayRecommend>
+    <Rank></Rank>
+    <Like></Like>
+    <Floor></Floor>
+    <Floor></Floor>
+    <Brand></Brand>
+  </div>
 </template>
 
 <script>
+import ListContainer from "@/pages/Home/ListContainer";
+import TodayRecommend from "@/pages/Home/TodayRecommend";
+import Rank from "@/pages/Home/Rank";
+import Like from "@/pages/Home/Like";
+import Floor from "@/pages/Home/Floor";
+import Brand from "@/pages/Home/Brand";
+
 export default {
-  name: "Home"
+  name: "Home",
+  components: {
+    ListContainer,
+    TodayRecommend,
+    Rank,
+    Like,
+    Floor,
+    Brand
+  }
 }
 </script>
 
 <style lang="less" scoped>
-.home {
-  color: red;
-}
+
 </style>
