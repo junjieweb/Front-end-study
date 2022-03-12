a42b19db8e3aa7abddefef9de743bbbaa92f9d90
junjieweb <297901657@qq.com>
2022-03-13 01:27:07 +0800

    update

--- 8-vue/shop-client/src/main.js
+++ 8-vue/shop-client/src/main.js
@@ -10,6 +10,14 @@ import '@/plugins/element' //加载element UI配置
 import "./mock/mockServer"
 import * as API from '@/api'
 
+import VueLazyload from 'vue-lazyload'
+import loading from '@/assets/images/loading.gif'
+// 在图片界面没有进入到可视范围前不加载, 在没有得到图片前先显示loading图片
+Vue.use(VueLazyload, { // 内部自定义了一个指令lazy
+    loading,  // 指定未加载得到图片之前的loading图片
+})
+
+
 // import 'swiper/css/swiper.min.css'; //所有组件都能用上这个样式
 
 Vue.config.productionTip = false
--- 8-vue/shop-client/src/pages/Search/index.vue
+++ 8-vue/shop-client/src/pages/Search/index.vue
@@ -86,7 +86,7 @@
                 <div class="list-wrap">
                   <div class="p-img">
                     <router-link :to="'/detail/'+ item.id">
-                      <img :src="item.defaultImg" alt=""/>
+                      <img v-lazy="item.defaultImg" />
                     </router-link>
                   </div>
                   <div class="price">
