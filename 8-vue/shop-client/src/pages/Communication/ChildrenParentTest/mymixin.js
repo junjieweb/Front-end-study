e89759003ae10fe4b94820882a22fbe8c06bc071
junjieweb <297901657@qq.com>
2022-03-14 21:31:35 +0800

    add commit

--- 8-vue/shop-client/src/pages/Communication/ChildrenParentTest/mymixin.js
+++
@@ -1,9 +0,0 @@
-export default {
-  methods: {
-    giveMoney(money){
-      this.money -= money
-      //爹的钱要增加
-      this.$parent.money += money
-    }
-  }
-}
--- 8-vue/shop-client/src/pages/Communication/EventTest/Event1.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div style="background: #ccc; height: 80px;">
-    <h2>Event1组件</h2>
-    <span>其它内容</span>
-  </div>
-</template>
