0f98ac10ad2797bc3da55df59b68ff7e238e8f99
junjieweb <297901657@qq.com>
2022-03-16 15:06:29 +0800

    update

--- 8-vue/vue-shangpinhui-admin/src/views/product/Attr/index.vue
+++ 8-vue/vue-shangpinhui-admin/src/views/product/Attr/index.vue
@@ -72,16 +72,19 @@
                 @blur="toLook(row)"
                 @keyup.native.enter="toLook(row)"
               />
-              <span v-else style="display: block">{{ row.valueName }}</span>
+              <span v-else style="display: block" @click="toEdit(row,$index)">{{ row.valueName }}</span>
             </template>
           </el-table-column>
           <el-table-column prop="prop" label="操作">
             <template v-slot="{row,$index}">
-              <el-button type="danger" icon="el-icon-delete" size="mini" />
+              <!--气泡确认框-->
+              <el-popconfirm :title="`确定删除${row.valueName}?`" @onConfirm="deleteAttrValue($index)">
+                <el-button slot="reference" type="danger" icon="el-icon-delete" size="mini" />
+              </el-popconfirm>
             </template>
           </el-table-column>
         </el-table>
-        <el-button type="primary">保存</el-button>
+        <el-button type="primary" @click="addOrUpdateAttr">保存</el-button>
         <el-button @click="isShowTable=true">取消</el-button>
       </div>
     </el-card>
@@ -152,6 +155,9 @@ export default {
         // 当前flag属性，响应式数据（数据变化视图跟着变化）
         flag: true
       })
+      this.$nextTick(() => {
+        this.$refs[this.attrInfo.attrValueList.length - 1].focus()
+      })
     },
     // 添加属性按钮的回调
     addAttr() {
@@ -172,6 +178,13 @@ export default {
       // 由于数据结构当中存在对象里面套数组，数组里面有套对象，因此需要使用深拷贝解决这类问题
       // 深拷贝，浅拷贝在面试的时候出现频率很高，切记达到手写深拷贝与浅拷贝
       this.attrInfo = cloneDeep(row)
+      // 在修改某一个属性的时候，将相应的属性值元素添加上flag这个标记
+      this.attrInfo.attrValueList.forEach(item => {
+        // 这样书写也可以给属性值添加flag自动，但是会发现视图不会跟着变化（因为flag不是响应式数据）
+        // 因为Vue无法探测普通的新增 property,这样书写的属性并非响应式属性（数据变化视图跟这边）
+        // 第一个参数:对象  第二个参数:添加新的响应式属性  第三个参数：新的属性的属性值
+        this.$set(item, 'flag', false)
+      })
     },
     // 失去焦点的事件---切换为查看模式，展示span
     toLook(row) {
@@ -190,9 +203,54 @@ export default {
           return row.valueName === item.valueName
         }
       })
-      if (isRepeat) return
+      if (isRepeat) {
+        this.$message('请勿添加重复属性')
+        return
+      }
       // 当前编辑模式变为查看模式【让input消失，显示span】
       row.flag = false
+    },
+    // 点击span的回调，变为编辑模式
+    toEdit(row, index) {
+      row.flag = true
+      // 获取input节点，实现自动聚焦
+      // 需要注意：点击span的时候，切换为input变为编辑模式，但是需要注意，对于浏览器而言，页面重绘与重拍耗时间的
+      // 点击span的时候，重绘重排一个input它是需要耗费事件，因此我们不可能一点击span立马获取到input
+      // $nextTick,当节点渲染完毕了，会执行一次
+      this.$nextTick(() => {
+        // 获取相应的input表单元素实现聚焦
+        this.$refs[index].focus()
+      })
+    },
+    // 气泡确认框确定按钮的回调
+    // 最新版本的ElementUI----2.15.6，目前模板中的版本号2.13.x
+    deleteAttrValue(index) {
+      // 当前删除属性值的操作是不需要发请求的
+      this.attrInfo.attrValueList.splice(index, 1)
+    },
+    // 保存按钮：进行添加属性或者修改属性的操作
+    async addOrUpdateAttr() {
+      // 整理参数:1,如果用户添加很多属性值，且属性值为空的不应该提交给服务器
+      // 提交给服务器数据当中不应该出现flag字段
+      this.attrInfo.attrValueList = this.attrInfo.attrValueList.filter(item => {
+        // 过滤掉属性值不是空的
+        if (item.valueName !== '') {
+          // 删除flag属性
+          delete item.flag
+          return true
+        }
+      })
+      try {
+        await this.$API.attr.reqAddOrUpdateAttr(this.attrInfo)
+        // 展示平台属性的信号量进行切换
+        this.isShowTable = true
+        // 提示消失
+        this.$message({ type: 'success', message: '保存成功' })
+        // 保存成功以后需要再次获取平台属性进行展示
+        await this.getAttrList()
+      } catch (e) {
+        this.$message('保存失败')
+      }
     }
   }
 }
