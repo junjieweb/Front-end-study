82253d35bcf05fbbb5216b0dcc7c3677bc68d501
junjieweb <297901657@qq.com>
2022-03-29 00:04:03 +0800

    update

---
+++ 8-vue/vue_review_demo/09_src_路由传参和路由守卫/pages/Category/Category.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>Category组件</h2>
+    <p>query参数：{{ this.$route.query }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Category"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ 8-vue/vue_review_demo/src/pages/Login/Login.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>登录页面</h1>
+</template>
+
+<script>
+export default {
+  name: "Login"
+}
+</script>
+
+<style scoped>
+
+</style>
