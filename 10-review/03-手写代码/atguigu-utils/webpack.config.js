398d795d5e8be9dbf298d0d84c738f66024e1ffa
junjieweb <297901657@qq.com>
2022-03-30 17:51:39 +0800

    update

---
+++ 10-review/03-手写代码/atguigu-utils/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  // mode: 'production',
+  entry: './src/index.js',
+  
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'atguigu-utils.js',
+    // filename: 'atguigu-utils.min.js',
+    libraryTarget: 'umd', // 打包生成库可以通过esm/commonjs/reqirejs的语法引入
+    library: 'aUtils' // 向外暴露的对象的名称
+  },
+  externals: { // 外部引入的包, 不需要打入当前包中
+    lodash: {
+      commonjs: 'lodash',
+      commonjs2: 'lodash',
+      amd: 'lodash',
+      root: '_' // 页面引入的全局变量
+    }
+  }
+}
