ab969739fe64e2d2b8a0a5e334ba90065ce09d26
junjieweb <297901657@qq.com>
2022-04-07 00:15:43 +0800

    update

--- 11-TypeScript/Snake/src/main.ts
+++ 11-TypeScript/Snake/src/main.ts
@@ -1,3 +1,7 @@
-
-// document.write('<h1>Hello TypeScript</h1>')
+// 引入样式
 import './index.less'
+import Food from "./moduls/Food";
+import ScorePanel from "./moduls/ScorePanel";
+
+const food = new Food()
+food.change()
