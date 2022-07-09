d439251c620bef5842c35f498e4a0d3097e62f11
Junjie <90107547+junjieweb@users.noreply.github.com>
2022-07-09 22:23:16 +0800

    feat: NodeJS Express 处理 404

--- 04-git-ES6-Node/06-express/express/01-HelloWorld/app.js
+++ 04-git-ES6-Node/06-express/express/01-HelloWorld/app.js
@@ -26,6 +26,10 @@ app.use(express.static('public'));
 app.use('/static', express.static('public'));
 app.use('/static', express.static(__dirname + '/public'));
 
+// 404
+app.use(function(req, res, next) {
+    res.status(404).send('Sorry cant find that!');
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
