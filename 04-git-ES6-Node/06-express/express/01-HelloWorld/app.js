db2f74e11d9b5bb55333f1ea3e0a4bd623ace48b
Junjie <90107547+junjieweb@users.noreply.github.com>
2022-07-09 22:23:58 +0800

    feat: NodeJS Express 设置错误处理程序

--- 04-git-ES6-Node/06-express/express/01-HelloWorld/app.js
+++ 04-git-ES6-Node/06-express/express/01-HelloWorld/app.js
@@ -31,6 +31,12 @@ app.use(function(req, res, next) {
     res.status(404).send('Sorry cant find that!');
 });
 
+//设置错误处理程序
+app.use(function(err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
