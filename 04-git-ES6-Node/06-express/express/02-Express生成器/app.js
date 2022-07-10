732a17837e75cf02c8ec3a7b88ec0b90f96f762f
Junjie <90107547+junjieweb@users.noreply.github.com>
2022-07-10 21:42:41 +0800

    refactor: Code Refactoring

--- 04-git-ES6-Node/06-express/express/02-Express生成器/app.js
+++ 04-git-ES6-Node/06-express/express/02-Express生成器/app.js
@@ -1,15 +1,15 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
-// view engine setup
+// view engine setup 查看引擎设置
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -22,14 +22,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+// catch 404 and forward to error handler 捕获 404 并转发到错误处理程序
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  // set locals, only providing error in development 设置本地人，只提供开发中的错误
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
