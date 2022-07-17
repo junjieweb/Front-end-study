c42da592739d805623e25edc4d4a325ced8bad4a
Junjie <90107547+junjieweb@users.noreply.github.com>
2022-07-17 20:42:01 +0800

    refactor: Code Refactoring

--- 08-vue/bilibili/data/05_其他/测试部署服务器/测试部署服务器/server.js
+++ 08-vue/bilibili/data/05_其他/测试部署服务器/测试部署服务器/server.js
@@ -3,15 +3,15 @@ const history = require('connect-history-api-fallback');
 
 const app = express()
 app.use(history())
-app.use(express.static(__dirname+'/static'))
+app.use(express.static(__dirname + '/static'))
 
-app.get('/person',(req,res)=>{
-	res.send({
-		name:'tom',
-		age:18
-	})
+app.get('/person', (req, res) => {
+    res.send({
+        name: 'tom',
+        age: 18
+    })
 })
 
-app.listen(5005,(err)=>{
-	if(!err) console.log('服务器启动成功了！')
+app.listen(5005, (err) => {
+    if (!err) console.log('服务器启动成功了！')
 })
