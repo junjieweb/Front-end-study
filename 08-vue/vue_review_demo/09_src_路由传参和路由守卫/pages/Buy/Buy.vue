e41153113933c889f5c22e9e4298ea9ca076be41
Junjie <90107547+junjieweb@users.noreply.github.com>
2022-07-18 20:59:10 +0800

    refactor: Code Refactoring

--- 08-vue/vue_review_demo/09_src_路由传参和路由守卫/pages/Buy/Buy.vue
+++ 08-vue/vue_review_demo/09_src_路由传参和路由守卫/pages/Buy/Buy.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
     <h2>Buy组件</h2>
-    <p>props获取的params参数：{{id}}</p>
+    <p>props获取的params参数：{{ id }}</p>
   </div>
 </template>
 
 <script>
 export default {
   name: "Buy",
-  props:['id']
+  props: ['id']
 }
 </script>
 
