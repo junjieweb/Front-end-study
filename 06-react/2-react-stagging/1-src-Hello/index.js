2b05d58613003a87929b2153846ea2667875179d
Junjie <90107547+junjieweb@users.noreply.github.com>
2022-08-25 21:22:21 +0800

    refactor: Code Refactoring

--- 06-react/2-react-stagging/1-src-Hello/index.js
+++ 06-react/2-react-stagging/1-src-Hello/index.js
@@ -1,9 +1,9 @@
-//引入React核心库
+// 引入React核心库
 import React from 'react';
-//引入ReactDOM
+// 引入ReactDOM
 import ReactDOM from 'react-dom';
-//引入App组件（App是所有组件的外壳组件，它包着所有的组件）
+// 引入App组件（App是所有组件的外壳组件，它包着所有的组件）
 import App from './App';
 
-//渲染App组件
+// 渲染App组件
 ReactDOM.render(<App/>, document.getElementById('root'));
