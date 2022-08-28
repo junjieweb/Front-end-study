9fd3c4472d0046fc9ad6ce41009d64a435ab92a6
Junjie <90107547+junjieweb@users.noreply.github.com>
2022-08-28 20:47:14 +0800

    refactor: Code Refactoring

--- 06-react/2-react-stagging/11-src-嵌套路由的使用/components/Header/index.jsx
+++ 06-react/2-react-stagging/11-src-嵌套路由的使用/components/Header/index.jsx
@@ -3,7 +3,6 @@ import React, {Component} from 'react';
 class Header extends Component {
     render() {
         return (
-
             <div className="row">
                 <div className="col-xs-offset-2 col-xs-8">
                     <div className="page-header"><h2>React Router Demo</h2></div>
