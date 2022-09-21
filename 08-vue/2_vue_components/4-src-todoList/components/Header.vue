e19d3e4bb25c25b30e84096652dad6b1686dd1bd
Junjie <90107547+junjieweb@users.noreply.github.com>
2022-09-21 21:29:22 +0800

    refactor: Code Refactoring

--- 08-vue/2_vue_components/4-src-todoList/components/Header.vue
+++ 08-vue/2_vue_components/4-src-todoList/components/Header.vue
@@ -8,19 +8,19 @@
 export default {
   name: "Header",
   /*props: {
-    //props的第二种写法，只是限定了传递过来属性值的类型
+    // props的第二种写法，只是限定了传递过来属性值的类型
     // addTodo:Function
 
-    //props的第三种写法，也是对象写法，只是比第二种对象写法更加严谨
+    // props的第三种写法，也是对象写法，只是比第二种对象写法更加严谨
     addTodo: {
-      //第三种，这是一个配置对象，它可以限定属性值的更多
+      // 第三种，这是一个配置对象，它可以限定属性值的更多
       type: Function,
       required: true
 
       /!*type: Number,
-      required: true,//是否必须传
-      default: 10, //默认值为10，不传的话默认值就是10
-      validator(value) { //验证器
+      required: true, // 是否必须传
+      default: 10, // 默认值为10，不传的话默认值就是10
+      validator(value) { // 验证器
         return value >= 0
       }*!/
     }
@@ -43,7 +43,7 @@ export default {
         }
         // this.addTodo(obj)
 
-        //自定义事件使用
+        // 自定义事件使用
         this.$emit('addTodo', obj)
       } else {
         alert('请输入合法数据')
