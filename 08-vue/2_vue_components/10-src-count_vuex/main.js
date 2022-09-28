fc63bbb3aed325ffe2a2659f04922c5517501c85
Junjie <90107547+junjieweb@users.noreply.github.com>
2022-09-28 21:59:57 +0800

    refactor: Code Refactoring

--- 08-vue/2_vue_components/10-src-count_vuex/main.js
+++ 08-vue/2_vue_components/10-src-count_vuex/main.js
@@ -7,6 +7,6 @@ Vue.config.productionTip = false
 new Vue({
     el: '#app',
     render: h => h(App),
-    //store对象在配置对象中配置使用，每个组件对象中都可以通过this.$store获取到我们的这个对象
+    // store对象在配置对象中配置使用，每个组件对象中都可以通过this.$store获取到我们的这个对象
     store
 })
