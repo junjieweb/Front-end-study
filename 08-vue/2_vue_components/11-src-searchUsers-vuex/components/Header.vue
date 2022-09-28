fd63ce42eea1f5d9970474a50282fb09e25b0aff
Junjie <90107547+junjieweb@users.noreply.github.com>
2022-09-28 22:00:21 +0800

    refactor: Code Refactoring

--- 08-vue/2_vue_components/11-src-searchUsers-vuex/components/Header.vue
+++ 08-vue/2_vue_components/11-src-searchUsers-vuex/components/Header.vue
@@ -21,9 +21,9 @@ export default {
       let keyword = this.keyword
       if (keyword.trim()) {
         // this.$bus.$emit('searchAjax', keyword)
-        //dispatch在触发的时候本质就是在调用actions中的方法，可以传递参数，但是只能传递一个
-        //如果要传递多个，请把多个参数封装为对象再传
-        //{keyword,name:'tom}
+        // dispatch在触发的时候本质就是在调用actions中的方法，可以传递参数，但是只能传递一个
+        // 如果要传递多个，请把多个参数封装为对象再传
+        // {keyword,name:'tom}
         this.$store.dispatch('searchAjax', keyword)
       } else {
         alert('请输入合法关键字')
