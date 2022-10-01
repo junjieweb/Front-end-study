a3594cc6ecc63bded2ef2540266c33e5afb82911
Junjie <90107547+junjieweb@users.noreply.github.com>
2022-10-01 23:15:56 +0800

    refactor: Code Refactoring

--- 08-vue/2_vue_components/src/router/index.js
+++ 08-vue/2_vue_components/src/router/index.js
@@ -12,17 +12,17 @@ Vue.use(VueRouter)
 
 export default new VueRouter({
     node: 'history',
-    //配置路由
+    // 配置路由
     routes: [
         {
             path: '/home',
-            component: Home, //注册路由组件
-            //子路由（二级路由）
+            component: Home, // 注册路由组件
+            // 子路由（二级路由）
             children: [
                 {
                     path: 'message',
                     component: Message,
-                    //子路由（三级路由）
+                    // 子路由（三级路由）
                     children: [{
                         // :msgid是用来接收路径传过来的params参数
                         path: 'msgdetail/:msgid',
@@ -33,9 +33,9 @@ export default new VueRouter({
                         props(route) {
                             return {msgid: route.params.msgid, content: route.query.content}
                         }
-                        //1.如果写的是布尔值，值为true代表会把传递过来的路径中的params参数映射为要显示的组件当中属性去使用
-                        //2.如果写对象,props是用来把需要自己额外传递的静态数据传映射为组件当中的属性，这个对象用法只能传递一些自己添加的额外数据
-                        //3.如果写函数，可以让我们自己把params参数和query参数一起映射为组件当中的属性
+                        // 1.如果写的是布尔值，值为true代表会把传递过来的路径中的params参数映射为要显示的组件当中属性去使用
+                        // 2.如果写对象,props是用来把需要自己额外传递的静态数据传映射为组件当中的属性，这个对象用法只能传递一些自己添加的额外数据
+                        // 3.如果写函数，可以让我们自己把params参数和query参数一起映射为组件当中的属性
 
                         // 匹配的同时把参数给解析出来，添加到当前这个路由对象中（理解）
                         /*params:{
