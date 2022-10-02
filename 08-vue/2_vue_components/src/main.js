7412179a3905c9a9981941b8546218fea2b70d28
Junjie <90107547+junjieweb@users.noreply.github.com>
2022-10-02 22:18:32 +0800

    refactor: Code Refactoring

--- 08-vue/2_vue_components/src/main.js
+++ 08-vue/2_vue_components/src/main.js
@@ -5,7 +5,7 @@ import router from "./router"
 Vue.config.productionTip = false
 
 new Vue({
-    //所有的组件当中都可以通过this.$router拿到路由器对象，还可以通过this.$route拿到当前路由对象
+    // 所有的组件当中都可以通过this.$router拿到路由器对象，还可以通过this.$route拿到当前路由对象
     router,
     render: h => h(App)
 }).$mount('#app')
