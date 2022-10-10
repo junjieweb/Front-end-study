5b4d24634c0dbb3f8f35cf5a8c389616e6126442
Junjie <90107547+junjieweb@users.noreply.github.com>
2022-10-10 20:34:17 +0800

    refactor: Code Refactoring

--- 08-vue/bilibili/vue2_test/03_src_props配置/components/Student.vue
+++ 08-vue/bilibili/vue2_test/03_src_props配置/components/Student.vue
@@ -22,25 +22,25 @@ export default {
       this.myAge++;
     },
   },
-  //简单声明接收
+  // 简单声明接收
   // props:['name','age','sex']
 
-  //接收的同时对数据进行类型限制
+  // 接收的同时对数据进行类型限制
   /* props:{
         name: String,
         age: Number,
         sex: String
     } */
 
-  //接收的同时对数据：进行类型限制、默认值的指定、必要性的限制
+  // 接收的同时对数据：进行类型限制、默认值的指定、必要性的限制
   props: {
     name: {
-      type: String, //name的类型是字符串
-      required: true, //name是必要的
+      type: String, // name的类型是字符串
+      required: true, // name是必要的
     },
     age: {
       type: Number,
-      default: 18, //默认值
+      default: 18, // 默认值
     },
     sex: {
       type: String,
