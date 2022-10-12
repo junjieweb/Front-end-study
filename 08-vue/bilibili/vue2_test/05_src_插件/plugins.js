26917a4bd920fa5c007e919b8419a2fcc69b3b83
Junjie <90107547+junjieweb@users.noreply.github.com>
2022-10-12 21:31:58 +0800

    refactor: Code Refactoring

--- 08-vue/bilibili/vue2_test/05_src_插件/plugins.js
+++ 08-vue/bilibili/vue2_test/05_src_插件/plugins.js
@@ -2,28 +2,28 @@ export default {
     install(Vue, x, y, z) {
         console.log(Vue, x, y, z)
 
-        //全局过滤器
+        // 全局过滤器
         Vue.filter('mySlice', function (value) {
             return value.slice(0, 4)
         })
 
-        //定义全局指令
+        // 定义全局指令
         Vue.directive('fbind', {
-            //指令与元素成功绑定时（一上来）
+            // 指令与元素成功绑定时（一上来）
             bind(element, binding) {
                 element.value = binding.value
             },
-            //指令所在元素被插入页面时
+            // 指令所在元素被插入页面时
             inserted(element, binding) {
                 element.focus()
             },
-            //指令所在的模板被重新解析时
+            // 指令所在的模板被重新解析时
             update(element, binding) {
                 element.value = binding.value
             }
         })
 
-        //定义混入
+        // 定义混入
         Vue.mixin({
             data() {
                 return {
@@ -33,7 +33,7 @@ export default {
             }
         })
 
-        //给Vue原型上添加一个方法（vm和vc就都能用了）
+        // 给Vue原型上添加一个方法（vm和vc就都能用了）
         Vue.prototype.hello = () => {
             alert('你好啊')
         }
