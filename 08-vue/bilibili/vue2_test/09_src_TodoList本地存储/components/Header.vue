7c6a9214534fbbeb14c107bbee4da5495121a05e
Junjie <90107547+junjieweb@users.noreply.github.com>
2022-10-14 22:21:16 +0800

    refactor: Code Refactoring

--- 08-vue/bilibili/vue2_test/09_src_TodoList本地存储/components/Header.vue
+++ 08-vue/bilibili/vue2_test/09_src_TodoList本地存储/components/Header.vue
@@ -19,21 +19,21 @@ export default {
   props: ["addTodo"],
   data() {
     return {
-      //收集用户输入的title
+      // 收集用户输入的title
       title: "",
     };
   },
   methods: {
     add() {
-      //校验数据
+      // 校验数据
       if (!this.title.trim()) {
         return alert("输入不能为空");
       }
-      //将用户的输入包装成一个todo对象
+      // 将用户的输入包装成一个todo对象
       const todo = {id: nanoid(), title: this.title, done: false};
-      //通知App组件去添加一个todo对象
+      // 通知App组件去添加一个todo对象
       this.addTodo(todo);
-      //清空输入
+      // 清空输入
       this.title = "";
     },
   },
