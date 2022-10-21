894160e61f23c37f224b91b89ece6737120bfc7a
Junjie <90107547+junjieweb@users.noreply.github.com>
2022-10-21 20:38:07 +0800

    refactor: Code Refactoring

--- 08-vue/bilibili/vue2_test/13_src_TodoList全局事件总线/components/Item.vue
+++ 08-vue/bilibili/vue2_test/13_src_TodoList全局事件总线/components/Item.vue
@@ -23,15 +23,15 @@ export default {
   name: "Item",
   props: ["todo"],
   methods: {
-    //勾选or取消勾选
+    // 勾选or取消勾选
     handleCheck(id) {
-      //通知App组件将对应的todo对象的done值取反
+      // 通知App组件将对应的todo对象的done值取反
       this.$bus.$emit("checkTodo", id);
     },
-    //删除
+    // 删除
     handleDelete(id) {
       if (confirm("确定删除吗？")) {
-        //通知App组件将对应的todo对象删除
+        // 通知App组件将对应的todo对象删除
         this.$bus.$emit("deleteTodo", id);
       }
     },
