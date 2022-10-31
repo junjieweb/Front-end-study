c50c8a6a3d434abe73d464132a2ef04d44568ab4
Junjie <90107547+junjieweb@users.noreply.github.com>
2022-10-31 21:54:11 +0800

    refactor: Code Refactoring

--- 08-vue/bilibili/vue2_test/24_src_求和案例_vuex版/store/index.js
+++ 08-vue/bilibili/vue2_test/24_src_求和案例_vuex版/store/index.js
@@ -1,17 +1,17 @@
-//该文件用于创建Vuex中最为核心的store
+// 该文件用于创建Vuex中最为核心的store
 import Vue from 'vue';
-//引入Vuex
+// 引入Vuex
 import Vuex from 'vuex';
-//应用Vuex插件
+// 应用Vuex插件
 Vue.use(Vuex)
 
-//准备state——用于存储数据
+// 准备state——用于存储数据
 const state = {
-    sum: 0, //当前的和
+    sum: 0, // 当前的和
 }
 
-//准备actions——用于响应组件中的动作
-//这个里面的方法可以包含if for 异步
+// 准备actions——用于响应组件中的动作
+// 这个里面的方法可以包含if for 异步
 const actions = {
     incrementOdd(context, value) {
         if (context.state.sum % 2) {
@@ -25,8 +25,8 @@ const actions = {
     }
 }
 
-//准备mutations——用于操作数据（state）
-//这个里面的方法不能包含 if for 异步，是直接操作的
+// 准备mutations——用于操作数据（state）
+// 这个里面的方法不能包含 if for 异步，是直接操作的
 const mutations = {
     INCREMENT(state, value) {
         state.sum += value
@@ -36,7 +36,7 @@ const mutations = {
     }
 }
 
-//创建并暴露store
+// 创建并暴露store
 export default new Vuex.Store({
     state,
     mutations,
