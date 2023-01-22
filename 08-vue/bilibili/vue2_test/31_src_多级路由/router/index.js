58f7bf24699fb4078294cb53d3ae2a0480fc28a0
Junjie <90107547+junjieweb@users.noreply.github.com>
2023-01-22 21:20:23 +0800

    refactor: Code Refactoring

--- 08-vue/bilibili/vue2_test/31_src_多级路由/router/index.js
+++ 08-vue/bilibili/vue2_test/31_src_多级路由/router/index.js
@@ -1,12 +1,12 @@
 // 该文件专门用于创建整个应用的路由器
 import VueRouter from 'vue-router';
-//引入组件
+// 引入组件
 import About from '@/pages/About';
 import Home from '@/pages/Home';
 import Message from '@/pages/Message';
 import News from '@/pages/News';
 
-//创建并暴露一个路由器
+// 创建并暴露一个路由器
 export default new VueRouter({
     routes: [
         {
